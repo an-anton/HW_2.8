d6e55b4a2c7adba12da0665845f7a92154db0342
Ananev Anton <an-anton88@mail.ru>
2021-11-12 14:46:04 +0300

    Initial Commit

---
+++ HW_2.8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.8
+//
+//  Created by Anton Anan'eV on 12.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
