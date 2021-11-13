4499c55bad1b8cabdce6ca5ff1d9f89eda0630cb
Ananev Anton <an-anton88@mail.ru>
2021-11-13 14:32:50 +0300

    Добавил логин вьюконтроллер и ссылку на маин сториборд

--- HW_2.8/ViewControllers/LoginViewController.swift
+++ HW_2.8/ViewControllers/LoginViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet var logInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        logInButton.layer.cornerRadius = logInButton.frame.height / 4
     }
 
 
