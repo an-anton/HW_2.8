629c7d86ed9c1581fff9a073f38fdf93ba980d63
Ananev Anton <an-anton88@mail.ru>
2021-11-15 01:10:00 +0300

    comit

--- HW_2.8/ViewControllers/LoginViewController.swift
+++ HW_2.8/ViewControllers/LoginViewController.swift
@@ -16,8 +16,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         logInButton.layer.cornerRadius = logInButton.frame.height / 4
-        //logInButton.isEnabled = false
-        //logInButton.backgroundColor = .clear
+       
     }
    
     
@@ -30,4 +29,8 @@ class LoginViewController: UIViewController {
     }
     
 }
-
+extension LoginViewController {
+    private func showAlert(massage: String, title: String) {
+        
+    }
+}
